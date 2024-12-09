b753e6203275122e708b079241b13b95ccec1010
Rinto de Vries <rinto@mainblades.com>
2024-12-09 22:11:17 +0100

    add day04 pt2

--- day04/day04.go
+++ day04/day04.go
@@ -6,6 +6,39 @@ import (
 	"os"
 )
 
+func parseInput(path string) ([][]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var result [][]byte
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := append([]byte(nil), scanner.Bytes()...) // What the hell go... this is a very nasty bug...
+		result = append(result, line)
+	}
+
+	return result, nil
+}
+
+func addPaddingToMatrix(unpadded [][]byte, padding int) [][]byte {
+	numRows := len(unpadded) + 2*padding
+	numCols := len(unpadded[0]) + 2*padding
+
+	result := make([][]byte, numRows)
+	for i := range result {
+		result[i] = make([]byte, numCols)
+	}
+
+	for i := range unpadded {
+		copy(result[i+padding][padding:], unpadded[i])
+	}
+
+	return result
+}
+
 func searchLeft(input [][]byte, target []byte, row int, col int) bool {
 	for i := 0; i < len(target); i++ {
 		if input[row][col-i] != target[i] {
@@ -75,6 +108,7 @@ func searchBottomRight(input [][]byte, target []byte, row int, col int) bool {
 			return false
 		}
 	}
+
 	return true
 }
 
@@ -85,7 +119,7 @@ func boolToInt(b bool) int {
 	return 0
 }
 
-func search(input [][]byte, target []byte, row int, col int) int {
+func searhPt1(input [][]byte, target []byte, row int, col int) int {
 	return boolToInt(searchLeft(input, target, row, col)) +
 		boolToInt(searchRight(input, target, row, col)) +
 		boolToInt(searchUp(input, target, row, col)) +
@@ -96,57 +130,46 @@ func search(input [][]byte, target []byte, row int, col int) int {
 		boolToInt(searchBottomRight(input, target, row, col))
 }
 
-func parseInput(path string) ([][]byte, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var result [][]byte
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := append([]byte(nil), scanner.Bytes()...) // What the hell go... this is a very nasty bug...
-		result = append(result, line)
-	}
-
-	return result, nil
-}
-
-func addPaddingToMatrix(unpadded [][]byte, padding int) [][]byte {
-	numRows := len(unpadded) + 2*padding
-	numCols := len(unpadded[0]) + 2*padding
-
-	result := make([][]byte, numRows)
-	for i := range result {
-		result[i] = make([]byte, numCols)
-	}
-
-	for i := range unpadded {
-		copy(result[i+padding][padding:], unpadded[i])
-	}
-
-	return result
-}
-
-func part1(path string) int {
-	unpadded, err := parseInput(path)
-	if err != nil {
-		panic(err)
-	}
-
+func part1(unpadded_input [][]byte) int {
 	target := []byte("XMAS")
-	padding := len(target)
-	padded := addPaddingToMatrix(unpadded, padding)
+	padded_input := addPaddingToMatrix(unpadded_input, len(target))
 	sum := 0
-	for row := padding; row < len(unpadded)+padding; row++ {
-		for col := padding; col < len(unpadded[0])+padding; col++ {
-			sum += search(padded, target, row, col)
+	for row := len(target); row < len(padded_input)-len(target); row++ {
+		for col := len(target); col < len(padded_input[0])-len(target); col++ {
+			sum += searhPt1(padded_input, target, row, col)
+		}
+	}
+	return sum
+}
+
+func searchPt2(input [][]byte, target []byte, row int, col int) int {
+	if (searchTopRight(input, target, row+1, col-1) || searchBottomLeft(input, target, row-1, col+1)) &&
+		(searchTopLeft(input, target, row+1, col+1) || searchBottomRight(input, target, row-1, col-1)) {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func part2(unpadded_input [][]byte) int {
+	target := []byte("SAM")
+	padded_input := addPaddingToMatrix(unpadded_input, len(target))
+
+	sum := 0
+	for row := len(target); row < len(padded_input)-len(target); row++ {
+		for col := len(target); col < len(padded_input[0])-len(target); col++ {
+			sum += searchPt2(padded_input, target, row, col)
 		}
 	}
 	return sum
 }
 
 func main() {
-	fmt.Println(part1("input.txt"))
+	unpadded_input, err := parseInput("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Answer to part1 = ", part1(unpadded_input))
+	fmt.Println("Answer to part2 = ", part2(unpadded_input))
 }
--- day04/day04_test.go
+++ day04/day04_test.go
@@ -3,24 +3,60 @@ package main
 import "testing"
 
 func TestSearchDirections(t *testing.T) {
-	got := part1("test_search_directions.txt")
+	unpadded_input, err := parseInput("test_search_directions.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	got := part1(unpadded_input)
 	want := 8
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
 
-func TestPadding(t *testing.T) {
-	got := part1("test_padding.txt")
+func TestPaddingPt1(t *testing.T) {
+	unpadded_input, err := parseInput("test_padding.txt")
+	if err != nil {
+		panic(err)
+	}
+	got := part1(unpadded_input)
 	want := 12
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+func TestPaddingPt2(t *testing.T) {
+	unpadded_input, err := parseInput("test_padding_part2.txt")
+	if err != nil {
+		panic(err)
+	}
+	got := part2(unpadded_input)
+	want := 4
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
 
 func TestMyTestInput(t *testing.T) {
-	got := part1("test_input.txt")
+	unpadded_input, err := parseInput("test_input.txt")
+	if err != nil {
+		panic(err)
+	}
+	got := part1(unpadded_input)
 	want := 18
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
+
+func TestMyTestInputPt2(t *testing.T) {
+	unpadded_input, err := parseInput("test_input_part2.txt")
+	if err != nil {
+		panic(err)
+	}
+	got := part2(unpadded_input)
+	want := 9
 	if got != want {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
